447b11af9efbbb091b05c6ef23189825f88be845
gummo <shaty3@hotmail.com>
2020-11-10 01:46:08 +0900

    プロジェクト ファイルを追加します。

---
+++ ref.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "ref.h"
+#include "comdef.h"
+
+//=====================================================================
+//型情報
+//=====================================================================
+typedef union {
+	aaa aaa;
+} type_info;
+
+//=====================================================================
+//型情報取得用
+//=====================================================================
+static const type_info _;
+
+//=====================================================================
+//フィールド情報
+//=====================================================================
+static const field_info f_aaa[] = {
+	const_field_val(aaa, ch),
+	const_field_val(aaa, s16),
+	const_field_val(aaa, i32),
+	const_field_ptr(aaa, pi32),
+	const_field_ptr(aaa, msg),
+	NULL,
+};
+
+//=====================================================================
+//構造体情報
+//=====================================================================
+static const struct_info structs[] = {
+	const_table(aaa),
+	NULL,
+};
+
+//=====================================================================
+//構造体情報取得
+//=====================================================================
+const struct_info* struct_get(const char* type_name) {
+	for (const struct_info* info = structs; NULL != info; info++) {
+		if (0 == strcmp(type_name, info->name)) {
+			return info;
+		}
+	}
+	return NULL;
+}
+
+//=====================================================================
+//フィールド情報取得
+//=====================================================================
+const field_info* field_get(const struct_info* self, const char* field_name) {
+	for (const field_info* info = self->fields; NULL != info; info++) {
+		if (0 == strcmp(field_name, info->name)) {
+			return info;
+		}
+	}
+	return NULL;
+}
+
+//=====================================================================
+//参照情報取得
+//=====================================================================
+void ref_get(const field_info* self, void* obj, ref* result) {
+	result->size = self->size;
+	result->ptr = (void*)((size_t)self->ptr - (size_t)&_ + (size_t)obj);
+}
+
+//=====================================================================
+//参照情報書込
+//=====================================================================
+void ref_set(ref* self, size_t val) {
+	switch (self->size) {
+	case 1:
+		*((uint8_t*)self->ptr) = (uint8_t)val;
+		break;
+	case 2:
+		*((uint16_t*)self->ptr) = (uint16_t)val;
+		break;
+	case 4:
+		*((uint32_t*)self->ptr) = (uint32_t)val;
+		break;
+	case 8:
+		*((uint64_t*)self->ptr) = (uint64_t)val;
+		break;
+	}
+}
---
+++ ref.h
@@ -0,0 +1,41 @@
+#ifndef __REF_H__
+#define __REF_H__
+
+//-----------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------
+
+//値
+typedef struct {
+	size_t size;
+	void* ptr;
+} ref;
+
+//フィールド情報
+typedef struct {
+	const char* name;
+	const void* ptr;
+	uint8_t size;
+} field_info;
+
+//構造体情報
+typedef struct {
+	const char* name;
+	const field_info* fields;
+} struct_info;
+
+extern const struct_info* struct_get(const char* type_name);
+extern const field_info* field_get(const struct_info* self, const char* field_name);
+extern void ref_get(const field_info* self, void* obj, ref* result);
+extern void ref_set(ref* self, size_t val);
+
+
+#define structs_count() (sizeof(structs) / sizeof(struct_info))
+#define const_field_val(type_name, field_name) { #field_name, &_. ## type_name ## . ## field_name, sizeof( _. ## type_name ## . ## field_name) }
+#define const_field_ptr(type_name, field_name) { #field_name, &_. ## type_name ## . ## field_name, sizeof(*_. ## type_name ## . ## field_name) }
+#define const_table(type_name) { #type_name, f_ ## type_name }
+
+
+
+
+#endif//__REF_H__
